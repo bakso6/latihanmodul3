535cd6c99156654d7db6732535b67e4d0194b250
bakso6 <127104338+bakso6@users.noreply.github.com>
2023-04-05 12:48:29 +0700

    Add files via upload

---
+++ latihanmdl3SD/src/latihanmdl3sd/MainQueue.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihanmdl3sd;
+
+/**
+ *
+ * @author ASUS
+ */
+class st {
+    
+}
